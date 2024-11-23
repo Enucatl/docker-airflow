6187e442c9723728830cfee61d581e7856c0a6cf
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2024-11-23 15:14:17 +0100

    email on failure

--- dags/hestan.py
+++ dags/hestan.py
@@ -6,7 +6,8 @@ default_args = {
     "depends_on_past": False,
     "start_date": datetime(2024, 1, 1),
     "retries": 1,
-    "retry_delay": timedelta(minutes=5),
+    "retry_delay": timedelta(minutes=30),
+    "email_on_failure": True,
 }
 
 
