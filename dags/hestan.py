4cca9e57bd62cef80ca68f5456d72fde64f55cd1
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2024-11-23 14:10:03 +0100

    create a database to store outputs

--- dags/hestan.py
+++ dags/hestan.py
@@ -23,60 +23,53 @@ with DAG(
     @task.virtualenv(
         task_id="scrape_and_analyze",
         requirements=[
-            "playwright",
             "beautifulsoup4",
         ],
         venv_cache_path=venv_cache_path,
     )
     def scrape_and_analyze():
-        import subprocess
-        import sys
-
         from bs4 import BeautifulSoup
         from playwright.sync_api import sync_playwright
 
-        process = subprocess.run(
-            [sys.executable, "-m", "playwright", "install"],
-            capture_output=True,
-            text=True,
-        )
-        print("Playwright installation output:", process.stdout)
-        print("Playwright installation errors:", process.stderr)
-
         # URL to scrape
-        urls = [
-            "https://hestanculinary.co/products/nanobond-induction-skillet-fry-pan?variant=43156722647296",
-            # "https://hestanculinary.co/products/nanobond-induction-skillet-fry-pan?variant=43156722680064",
-        ]
+        urls = {
+            "small": "https://hestanculinary.co/products/nanobond-induction-skillet-fry-pan?variant=43156722647296",
+            "medium": "https://hestanculinary.co/products/nanobond-induction-skillet-fry-pan?variant=43156722680064",
+        }
 
         # Scrape the webpage
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-        }
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 
         product_infos = []
         with sync_playwright() as p:
-            for url in urls:
+            for name, url in urls.items():
                 # Launch browser
                 browser = p.chromium.launch(headless=True)
-                page = browser.new_page()
+                page = browser.new_page(user_agent=user_agent)
 
                 # Go to URL and wait for content to load
                 page.goto(url)
-                page.wait_for_load_state("networkidle")
+                class_ = "product__price"
+                page.wait_for_selector(f".{class_}", state="visible", timeout=2000)
                 content = page.content()
 
                 browser.close()
 
                 # Parse the HTML content
                 soup = BeautifulSoup(content, "html.parser")
-                print(soup)
 
+                price = (
+                    soup.find("span", class_=class_).text.strip()
+                    if soup.find("span", class_=class_)
+                    else ""
+                )
+                currency = price[0]
+                price = float(price[1:])
                 # Extract the main content (you might need to adjust these selectors based on the actual page structure)
                 product_info = {
-                    "price": soup.find("span", class_="product__price").text.strip()
-                    if soup.find("span", class_="price")
-                    else "",
+                    "name": name,
+                    "price": price,
+                    "currency": currency,
                 }
                 product_infos.append(product_info)
         return product_infos
