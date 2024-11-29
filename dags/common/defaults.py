60f364da676ca5070865f489307d89fa065396fe
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2024-11-29 19:51:01 +0100

    add retry by default

--- dags/common/defaults.py
+++ dags/common/defaults.py
@@ -1,8 +1,10 @@
 import os
+from datetime import timedelta
 
 default_args = {
     "depends_on_past": False,
     "email": [os.environ.get("AIRFLOW__SMTP__SMTP_USER")],  # Replace with your email
-    "retries": 0,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=30),
     "venv_cache_path": "/opt/airflow/venv",
 }
