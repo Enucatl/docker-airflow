64a09a7eea373d90f8030b67b19cc0eb283e2312
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2024-12-01 10:16:46 +0100

    no email on retry

--- dags/common/defaults.py
+++ dags/common/defaults.py
@@ -4,6 +4,7 @@ from datetime import timedelta
 default_args = {
     "depends_on_past": False,
     "email": [os.environ.get("AIRFLOW__SMTP__SMTP_USER")],  # Replace with your email
+    "email_on_retry": False,
     "retries": 1,
     "retry_delay": timedelta(minutes=30),
     "venv_cache_path": "/opt/airflow/venv",
