7fc39bb7b1943f882129d7509c619307dc8cf9a7
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2025-07-01 20:13:29 +0200

    use ldap instead of authelia

--- config/webserver_config.py
+++ config/webserver_config.py
@@ -1,49 +1,57 @@
-from flask_appbuilder.const import AUTH_REMOTE_USER
+import os
 
-from airflow.providers.fab.auth_manager.security_manager.override import (
-    FabAirflowSecurityManagerOverride,
-)
+from flask_appbuilder.const import AUTH_LDAP
 
-AUTH_TYPE = AUTH_REMOTE_USER
+AUTH_TYPE = AUTH_LDAP
 AUTH_REMOTE_USER_ENV_VAR = "HTTP_REMOTE_USER"
 AUTH_ROLE_ADMIN = "Admin"
 AUTH_USER_REGISTRATION = True
 AUTH_USER_REGISTRATION_ROLE = AUTH_ROLE_ADMIN
 
+AUTH_LDAP_BIND_USER = "uid=airflow,cn=users,cn=accounts,dc=home,dc=arpa"
+AUTH_LDAP_BIND_PASSWORD = os.environ["AIRFLOW_LDAP_PASSWORD"]
 
-class RemoteUserFixFabAirflowSecurityManagerOverride(FabAirflowSecurityManagerOverride):
-    def __init__(self, appbuilder):
-        self.auth_remote_user_env_var = AUTH_REMOTE_USER_ENV_VAR
-        super().__init__(appbuilder)
+# This is the base DN for your user searches.
+# Combining your base_dn and additional_users_dn
+AUTH_LDAP_SEARCH_BASE = "cn=users,cn=compat,dc=home,dc=arpa"
 
-    def auth_user_remote_user(self, username):
-        """
-        REMOTE_USER user Authentication
+# The filter to find a user. The user-provided username will be substituted for `%(username)s`.
+# Translated from: (&({username_attribute}={input})(objectClass=posixAccount))
+# We use 'uid' because you specified it as the username attribute.
+AUTH_LDAP_SEARCH_FILTER = "(&(objectClass=posixAccount)(uid=%(username)s))"
 
-        :param username: user's username for remote auth
-        :type self: User model
-        """
-        user = self.find_user(username=username)
+# The attribute that is used as the username for the Airflow user.
+AUTH_LDAP_UID_FIELD = "uid"
 
-        # User does not exist, create one if auto user registration.
-        if user is None and self.auth_user_registration:
-            user = self.add_user(
-                # All we have is REMOTE_USER, so we set
-                # the other fields to blank.
-                username=username,
-                first_name=username,
-                last_name="-",
-                email=username + "@email.notfound",
-                role=self.find_role(self.auth_user_registration_role),
-            )
+# A mapping from LDAP attributes to User model fields.
+# You wanted the display_name to be the uid. We map it to first_name.
+AUTH_LDAP_FIRSTNAME_FIELD = "uid"
+AUTH_LDAP_LASTNAME_FIELD = "uid"  # 'sn' is the standard for surname
+AUTH_LDAP_EMAIL_FIELD = "mail"
 
-        # If user does not exist on the DB and not auto user registration,
-        # or user is inactive, go away.
-        elif user is None or (not user.is_active):
-            return None
+# =================================================================
+# Group-Based Role Mapping
+# =================================================================
+# Enable role mapping based on LDAP groups
+AUTH_ROLES_SYNC_AT_LOGIN = True
 
-        self.update_user_auth_stat(user)
-        return user
+# The base DN for your group searches.
+# Combining your base_dn and additional_groups_dn
+AUTH_LDAP_GROUP_SEARCH_BASE = "cn=groups,cn=compat,dc=home,dc=arpa"
 
+# The filter to find the groups a user belongs to.
+# The user's full DN will be substituted for `%(user_dn)s`, their username for `%(username)s`.
+# Your filter `(&(memberUid={input})(objectClass=posixGroup))` uses the username (uid).
+AUTH_LDAP_GROUP_SEARCH_FILTER = "(&(objectClass=posixGroup)(memberUid=%(username)s))"
 
-SECURITY_MANAGER_CLASS = RemoteUserFixFabAirflowSecurityManagerOverride
+# The attribute on the group object that contains the group's name.
+AUTH_LDAP_GROUP_TITLE_ATTR = "cn"
+
+# A mapping from LDAP Group DNs or group names (cn) to Airflow roles.
+# The keys are the 'cn' from your LDAP groups.
+# The values are the names of the Airflow roles (e.g., "Admin", "Op", "User", "Viewer").
+AUTH_LDAP_ROLE_MAPPING = {
+    "admins": "Admin",
+    "ops": "Op",
+    "users": "User",
+}
