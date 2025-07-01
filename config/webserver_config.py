f151c196c4ad17066cfd7400992e4d437649e594
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2025-07-01 17:42:07 +0200

    trying to fix user authentication

--- config/webserver_config.py
+++ config/webserver_config.py
@@ -1,19 +1,49 @@
-from typing import Any, Callable
-
-from flask import current_app
 from flask_appbuilder.const import AUTH_REMOTE_USER
 
-
-class CustomMiddleware:
-    def __init__(self, wsgi_app: Callable) -> None:
-        self.wsgi_app = wsgi_app
-
-    def __call__(self, environ: dict, start_response: Callable) -> Any:
-        if "HTTP_REMOTE_USER" in environ:
-            environ["REMOTE_USER"] = environ["HTTP_REMOTE_USER"]
-        return self.wsgi_app(environ, start_response)
-
-
-current_app.wsgi_app = CustomMiddleware(current_app.wsgi_app)
+from airflow.providers.fab.auth_manager.security_manager.override import (
+    FabAirflowSecurityManagerOverride,
+)
 
 AUTH_TYPE = AUTH_REMOTE_USER
+AUTH_REMOTE_USER_ENV_VAR = "HTTP_REMOTE_USER"
+AUTH_ROLE_ADMIN = "Admin"
+AUTH_USER_REGISTRATION = True
+AUTH_USER_REGISTRATION_ROLE = AUTH_ROLE_ADMIN
+
+
+class RemoteUserFixFabAirflowSecurityManagerOverride(FabAirflowSecurityManagerOverride):
+    def __init__(self, appbuilder):
+        self.auth_remote_user_env_var = AUTH_REMOTE_USER_ENV_VAR
+        super().__init__(appbuilder)
+
+    def auth_user_remote_user(self, username):
+        """
+        REMOTE_USER user Authentication
+
+        :param username: user's username for remote auth
+        :type self: User model
+        """
+        user = self.find_user(username=username)
+
+        # User does not exist, create one if auto user registration.
+        if user is None and self.auth_user_registration:
+            user = self.add_user(
+                # All we have is REMOTE_USER, so we set
+                # the other fields to blank.
+                username=username,
+                first_name=username,
+                last_name="-",
+                email=username + "@email.notfound",
+                role=self.find_role(self.auth_user_registration_role),
+            )
+
+        # If user does not exist on the DB and not auto user registration,
+        # or user is inactive, go away.
+        elif user is None or (not user.is_active):
+            return None
+
+        self.update_user_auth_stat(user)
+        return user
+
+
+SECURITY_MANAGER_CLASS = RemoteUserFixFabAirflowSecurityManagerOverride
