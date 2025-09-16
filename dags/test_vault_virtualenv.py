43ff55ec457ce0a5a60ddae4aec766fe06ecdbe9
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2025-09-16 11:46:11 +0200

    add test  vault dags

---
+++ dags/test_vault_virtualenv.py
@@ -0,0 +1,38 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.decorators import task
+from airflow.hooks.base import BaseHook
+
+
+default_args = {
+    "depends_on_past": False,
+    "retries": 0,
+    "venv_cache_path": "/opt/airflow/venv",
+}
+
+
+with DAG(
+    "test_vault_virtualenv",
+    default_args=default_args,
+    description="Test vault access for connections",
+    schedule="@once",
+    start_date=datetime(2025, 8, 17),
+) as dag:
+
+    @task
+    def test_connection():
+        connection = BaseHook.get_connection("stva")
+        print(connection.host)
+
+    @task.virtualenv(
+        requirements=[],
+    )
+    def test_virtualenv(logical_date):
+        from airflow.hooks.base import BaseHook
+
+        connection = BaseHook.get_connection("stva")
+        print(connection.host)
+
+    test_connection()
+    test_virtualenv()
